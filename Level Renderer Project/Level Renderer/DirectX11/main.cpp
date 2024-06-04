0fbdbb9c2eca57428799d1eade68468c3533229b
Jazmine Chargualaf <71017555+CodeWithJazmine@users.noreply.github.com>
2024-06-03 20:12:48 -0700

    Revert "Refactored Render class to RenderManager"
    This reverts commit cf411b3e4abb99e258887caac51ace23f04c5d2f.

--- Level Renderer Project/Level Renderer/DirectX11/main.cpp
+++ Level Renderer Project/Level Renderer/DirectX11/main.cpp
@@ -23,6 +23,7 @@ using namespace GRAPHICS;
 // lets pop a window and use D3D11 to clear to a green screen
 int main()
 {
+
 	GWindow win;
 	GEventResponder msgs;
 	GDirectX11Surface d3d11;
@@ -39,7 +40,7 @@ int main()
 		win.Register(msgs);
 		if (+d3d11.Create(win, GW::GRAPHICS::DEPTH_BUFFER_SUPPORT))
 		{
-			RenderManager renderer(win, d3d11);
+			Renderer renderer(win, d3d11);
 			while (+win.ProcessWindowEvents())
 			{
 				IDXGISwapChain* swap;
@@ -53,7 +54,7 @@ int main()
 				{
 					con->ClearRenderTargetView(view, clr);
 					con->ClearDepthStencilView(depth, D3D11_CLEAR_DEPTH, 1, 0);
-					//renderer.Update();
+					renderer.Update();
 					renderer.Render();
 					swap->Present(1, 0);
 					// release incremented COM reference counts
